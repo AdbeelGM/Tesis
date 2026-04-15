ec842fb9dbd4fc941909d2a8b1848ee0a09b0143
AdbeelGM <162487138+AdbeelGM@users.noreply.github.com>
2026-04-15 10:46:30 -0600

    Imagenes nuevas

--- js/level-config/level-1.js
+++ js/level-config/level-1.js
@@ -2,8 +2,6 @@ export const LEVEL_CONFIG = {
   level: 1,
   categoria: "abecedario",
   dificultad: 1,
-
-  // Cantidad de preguntas por tipo EN EL MISMO NIVEL
   mix: {
     multiple_choice: 1,
     true_false: 1,
