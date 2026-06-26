895ef9f94d3c56bccf45022b9bcc15aa6e9dc8cc
AdbeelGM <162487138+AdbeelGM@users.noreply.github.com>
2026-06-25 21:54:58 -0600

    Revert "base de datos"
    This reverts commit 689db2822a2434c488c78bf4723ba478990a3e15.

--- backend/db.js
+++ backend/db.js
@@ -6,11 +6,10 @@
 import mysql from "mysql2/promise";
 
 export const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  port: process.env.DB_PORT || 3306,
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "root",
-  database: process.env.DB_NAME || "lsm_gamificada",
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "lsm_gamificada",
   waitForConnections: true,
   connectionLimit: 10
 });
